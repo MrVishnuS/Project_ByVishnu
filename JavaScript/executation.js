a99e2698d40b910a4f8bd9eb249ea0e893ba53e4
MrVishnuS <vishnualfie@gmail.com>
2023-06-25 23:49:46 +0530

    Javascript

---
+++ JavaScript/executation.js
@@ -0,0 +1,41 @@
+// console.log(a);
+// var a=30;
+
+
+// // console.log(c);
+// const c =30;
+
+
+// function call(a,b,c){
+//     var result=a+b+c;
+
+//     return result;
+// }
+
+
+// console.log(result);
+// let result =call(10, 20, 30);
+
+
+// console.log(result);
+// console.log(a);
+// var a=40;
+// console.log(a);
+// let a = 40;
+
+// console.log(a);
+
+// a=60;
+
+
+// // console.log(b);
+
+// const b = 40;
+
+// console.log(b);
+// 
+let a = 40;
+var a = 30;
+
+console.log(a);
+
---
+++ JavaScript/practise.js
@@ -0,0 +1,22 @@
+let user = {
+
+    name: "Karan",
+    
+    address: { personal: "Delhi", area: "CivilLines" },
+    
+    office: { state: "Mumbai", area: { landmark: "Bandra" } },
+    
+};
+// {
+
+// name: 'Karan',
+
+// personal: 'Delhi',
+
+// area: 'CivilLines',
+
+// state: 'Mumbai',
+
+// landmark: 'Bandra'
+
+// }
